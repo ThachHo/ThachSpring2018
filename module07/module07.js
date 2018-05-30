b3100c1abb5ac9acee04546596951aa4b170de64
ThachHo <38049149+ThachHo@users.noreply.github.com>
2018-05-29 18:13:06 -0700

    Update module07.js

--- module07/module07.js
+++ module07/module07.js
@@ -34,5 +34,3 @@ elHotel3.textContent=details3;
 /* 
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
-*/
-greetMe("World");
